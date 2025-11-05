4de1461a3c50e2b75d0343b76f5d21f786c0bbcf
ak35kdjhv <roxzoakaro@gmail.com>
2025-11-05 15:21:17 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let Image,hero = document.getElementById('Expro')
+        // let text = document.getElementById('text')
+        let btn = document.getElementById('btn')
+
+        window.addEventListener('scroll', function(){
+            let value = window.scrollY;
+        })
