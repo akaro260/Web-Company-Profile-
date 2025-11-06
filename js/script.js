22806dc78dd4642224154cf0f4b0e557a1f642f1
ak35kdjhv <roxzoakaro@gmail.com>
2025-11-06 09:02:34 +0700

    background blue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-// let Image,hero = document.getElementById('Expro')
-        // let text = document.getElementById('text')
-        let btn = document.getElementById('btn')
+// // let Image,hero = document.getElementById('Expro')
+//         // let text = document.getElementById('text')
+//         let btn = document.getElementById('btn')
 
-        window.addEventListener('scroll', function(){
-            let value = window.scrollY;
-        })
+//         window.addEventListener('scroll', function(){
+//             let value = window.scrollY;
+//         })
+        window.addEventListener("scroll", function (){
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled")
+    } else {
+        (navbar.classList.remove("scrolled"))
+}
+});
