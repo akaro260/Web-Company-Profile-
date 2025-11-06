d20d3c95b1e1baaf01266f2b4a5261e82ce35fde
ak35kdjhv <roxzoakaro@gmail.com>
2025-11-06 09:14:41 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // // let Image,hero = document.getElementById('Expro')
-//         // let text = document.getElementById('text')
-//         let btn = document.getElementById('btn')
+        // let text = document.getElementById('text')
+        // let btn = document.getElementById('btn')
 
-//         window.addEventListener('scroll', function(){
-//             let value = window.scrollY;
-//         })
+        // window.addEventListener('scroll', function(){
+        //     let value = window.scrollY;
+        // })
         window.addEventListener("scroll", function (){
     const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 0) {
         navbar.classList.add("scrolled")
     } else {
         (navbar.classList.remove("scrolled"))
